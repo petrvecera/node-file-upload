835707bcaabaf5cdd4a28d67b532a8124abfa5ad
petrvecera <vecera.petr@gmail.com>
2018-03-31 20:01:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Example code how to handle large file upload in Node
+
+Aka how to stream the data directly to disk while they are beeing uploaded.
+
+**You can read more info https://pagep.net/2018/03/31/how-to-handle-large-file-upload-with-nodejs-express-server/**
+
+But the code should be pretty self-explanatory...
+
+## Run the code
+
+1. `git clone https://github.com/petrvecera/node-file-upload.git`
+2. `cd node-file-upload`
+3. `yarn install`
+4. `yarn start`
+
+You can use also `npm` instead of `yarn`.
---
+++ main.js
@@ -0,0 +1,53 @@
+const express = require('express');         // Express Web Server
+const busboy = require('connect-busboy');   // Middleware to handle the file upload https://github.com/mscdex/connect-busboy
+const path = require('path');               // Used for manipulation with path
+const fs = require('fs-extra');             // Classic fs
+
+const app = express(); // Initialize the express web server
+app.use(busboy({
+    highWaterMark: 2 * 1024 * 1024, // Set 2MiB buffer
+})); // Insert the busboy middle-ware
+
+const uploadPath = path.join(__dirname, 'fu/'); // Register the upload path
+fs.ensureDir(uploadPath); // Make sure that he upload path exits
+
+
+/**
+ * Create route /upload which handles the post request
+ */
+app.route('/upload').post((req, res, next) => {
+
+    req.pipe(req.busboy); // Pipe it trough busboy
+
+    req.busboy.on('file', (fieldname, file, filename) => {
+        console.log(`Upload of '${filename}' started`);
+
+        // Create a write stream of the new file
+        const fstream = fs.createWriteStream(path.join(uploadPath, filename));
+        // Pipe it trough
+        file.pipe(fstream);
+
+        // On finish of the upload
+        fstream.on('close', () => {
+            console.log(`Upload of '${filename}' finished`);
+            res.redirect('back');
+        });
+    });
+});
+
+
+/**
+ * Serve the basic index.html with upload form
+ */
+app.route('/').get((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="fileToUpload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+});
+
+const server = app.listen(3200, function () {
+    console.log(`Listening on port ${server.address().port}`);
+});
